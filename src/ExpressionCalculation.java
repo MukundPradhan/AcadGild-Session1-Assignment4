a5dc4007757690a24cdd76aadb3f3ab589146325
MukundPradhan <replyme.mmp@gmail.com>
2016-01-27 02:52:03 +0530

    Adding Class

---
+++ src/ExpressionCalculation.java
@@ -0,0 +1,18 @@
+
+public class ExpressionCalculation {
+	public static void main(String args[]){
+		int x=5, y=10;
+		int Exp1,Exp2,Exp3,Exp4;
+		
+		Exp1=x+y*2;
+		Exp2=x-y+2;
+		Exp3=(x+y)*2;
+		Exp4=y%x;
+		
+	   System.out.println("The value of  x+y*2 = " + Exp1);
+	   System.out.println("The value of  x-y+2 = " + Exp2);
+	   System.out.println("The value of  (x+y)*2 = " + Exp3);
+	   System.out.println("The value of  y%x = " + Exp4);
+	}
+
+}
